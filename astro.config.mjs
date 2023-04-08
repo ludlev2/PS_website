b055cb1e785a878c187717f547f1eb6dd74ed2bc
Adithya Ravichandran <46827614+Ad1thya-R@users.noreply.github.com>
2023-04-08 15:05:45 +0200

    animation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,16 @@
 import { defineConfig } from 'astro/config';
-import Unfonts from 'unplugin-fonts/astro'
+import Unfonts from 'unplugin-fonts/astro';
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    Unfonts({
-      google: {
-        families: [
-          'Cabin',
-          {
-            name: 'Open Sans',
-            styles: 'wght@300;400',
-          },
-        ],
-      },
-    })
-  ]
+  integrations: [Unfonts({
+    google: {
+      families: ['Cabin', {
+        name: 'Open Sans',
+        styles: 'wght@300;400'
+      }]
+    }
+  }), image()]
 });
