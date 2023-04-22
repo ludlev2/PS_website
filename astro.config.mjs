3c7446e051c0470c995f8f92a38efbf55d64a5df
Adithya Ravichandran <46827614+Ad1thya-R@users.noreply.github.com>
2023-04-22 13:46:15 +0200

    done without the buttons working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import Unfonts from 'unplugin-fonts/astro';
-
 import image from "@astrojs/image";
+
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [Unfonts({
     google: {
       families: ['Cabin', {
