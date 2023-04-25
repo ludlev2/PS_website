986a79c2e988c8260ce86399bcea38b38f4d4ca1
ludlev2 <ludanyi.levente@gmail.com>
2023-04-25 22:21:57 +0200

    updated rm static

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercelStatic(),
+  //output: 'server',
+  //adapter: vercelStatic(),
   integrations: [Unfonts({
     google: {
       families: ['Cabin', {
